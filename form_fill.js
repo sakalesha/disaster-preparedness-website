dc93e25afc23132594fc970c687f8762bf5df0a7
sakalesha <149064711+sakalesha@users.noreply.github.com>
2024-08-11 09:30:51 +0530

    Add files via upload

---
+++ form_fill.js
@@ -0,0 +1,6 @@
+
+document.getElementById('disasterForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted successfully!');
+    // You can add additional JavaScript here to handle the form data
+});
